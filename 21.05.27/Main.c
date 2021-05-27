b67b612912395d70d09857f836988bb20e1e31ba
명균 양 <krymk1212@naver.com>
2021-05-27 10:29:31 +0900

    21.05.27

--- 21.05.27/Main.c
+++ 21.05.27/Main.c
@@ -1,8 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+	/*
 	printf("Hello World\n\n");
 
 	printf("나는 %d살입니다.\n", 26);
@@ -10,7 +13,7 @@ int main()
 	printf("나는 %c입니다.\n", 'A');
 	printf("나는 %s입니다.\n", "양명균");
 
-	printf("5+9=%d\n", 5 + 9);
+	printf("\n5+9=%d\n", 5 + 9);
 
 	int Level;
 	Level = 10;
@@ -26,20 +29,51 @@ int main()
 	char c = 'A';		//문자
 
 	printf("\n2 x 1 = %d\n", 2);
-	printf("2 x 2 = %d\n", 2*2);
-	printf("2 x 3 = %d\n", 2*3);
-	printf("2 x 4 = %d\n", 2*4);
-	printf("2 x 5 = %d\n", 2*5);
-	printf("2 x 6 = %d\n", 2*6);
-	printf("2 x 7 = %d\n", 2*7);
-	printf("2 x 8 = %d\n", 2*8);
-	printf("2 x 9 = %d\n", 2*9);
+	printf("2 x 2 = %d\n", 2 * 2);
+	printf("2 x 3 = %d\n", 2 * 3);
+	printf("2 x 4 = %d\n", 2 * 4);
+	printf("2 x 5 = %d\n", 2 * 5);
+	printf("2 x 6 = %d\n", 2 * 6);
+	printf("2 x 7 = %d\n", 2 * 7);
+	printf("2 x 8 = %d\n", 2 * 8);
+	printf("2 x 9 = %d\n", 2 * 9);
+
+	
+	for (int i = 2; i <= 2; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			printf("%d x %d = %d\n", i, j, i * j);
+		}
+	}
+	
 
 	int age = 26;
 	float weight = 68.5f;
 	float height = 168.9f;
 
 	printf("\n나이:%d\n체중:%.1f\n신장:%.1f\n", age, weight, height);
+
+	int i1 = 10;
+	int i2 = 3;
+	float result1 = i1 / i2; // 정수와 정수
+	printf("%f\n",result1);
+
+	int i3 = 10;
+	float f1 = 3.5f;
+	float result2 = i3 / f1; // 정수와 실수
+	printf("%f\n", result2);
+
+	float result3 = f1 / i3; // 실수와 정수
+	printf("%f\n", result3);
+
+	float result4 = result1 / result2; // 실수와 실수
+	printf("%f\n", result4);
+	*/
+
+	//==============================================================================
+
+
 
 	return 0;
 }
