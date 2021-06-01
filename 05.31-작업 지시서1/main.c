32f996dfbca3612c3d1e5b621f2170159b095e70
명균 양 <krymk1212@naver.com>
2021-06-01 09:10:05 +0900

    21.06.01

--- 05.31-작업 지시서1/main.c
+++ 05.31-작업 지시서1/main.c
@@ -4,13 +4,43 @@
 
 int main()
 {
-	int i, j;
-	printf("숫자 두개를 입력해주세요.(1~9)");
-	scanf(" %d %d", i, j);
+	int insert, insert2;
+	printf("2개의 숫자 입력 : ");
+	scanf(" %d %d", &insert, &insert2);
 
-	if (i > 9 || j >> 9 || i < 0 || j < 0)
+	if (((insert >= 1) && (insert <= 9)) && ((insert2 >= 1) && (insert2 <= 9)))
+	{
+		if (insert >= insert2)
+		{
+			for (int i = insert2; i <= insert; i++)
+			{
+				printf("%d단 시작\n", i);
+
+				for (int j = 1; j <= 9; j++)
+				{
+					printf("%d x %d = %d\t", i, j, i * j);
+				}
+				printf("\n");
+			}
+		}
+		else
+		{
+			for (int i = insert; i <= insert2; i++)
+			{
+				printf("%d단 시작\n", i);
+
+				for (int j = 1; j <= 9; j++)
+				{
+					printf("%d x %d = %d\t", i, j, i * j);
+				}
+				printf("\n");
+			}
+		}
+	}
+	else
 	{
 		return 0;
 	}
+
 	return 0;
 }
